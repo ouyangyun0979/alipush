c6e7a403d78c6924eaa39dfbbe05adcdc0b45f13
yangyun <775292749@qq.com>
2016-03-22 20:32:23 +0800

    Signed-off-by: yangyun <775292749@qq.com>

--- alipush/src/org/yun/alipush/MainActivity.java
+++ alipush/src/org/yun/alipush/MainActivity.java
@@ -1,11 +1,9 @@
 package org.yun.alipush;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -13,24 +11,6 @@ public class MainActivity extends ActionBarActivity {
 		System.out.println("-------------------------开始我的git和yunpush");
 		System.out.println("-------------------------git上修改");
 		setContentView(R.layout.activity_main);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true; 
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
+		
 	}
 }
