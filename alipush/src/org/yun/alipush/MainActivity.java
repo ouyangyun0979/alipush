93c71fb539db0d38301d36ef6409d7c34edbe543
ouyangyun <775292749@qq.com>
2016-03-22 17:29:30 +0800

    测试

--- alipush/src/org/yun/alipush/MainActivity.java
+++ alipush/src/org/yun/alipush/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends ActionBarActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		 
 		setContentView(R.layout.activity_main);
 	}
 
@@ -17,7 +18,7 @@ public class MainActivity extends ActionBarActivity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+		return true; 
 	}
 
 	@Override
