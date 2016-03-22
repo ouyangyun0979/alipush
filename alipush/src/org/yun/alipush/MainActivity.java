0f2f086acffeb63df85f49aadfc5968b0d04192c
ouyangyun0979 <775292749@qq.com>
2016-03-22 17:33:00 +0800

    Update MainActivity.java

--- alipush/src/org/yun/alipush/MainActivity.java
+++ alipush/src/org/yun/alipush/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		System.out.println("-------------------------开始我的git和yunpush");
+		System.out.println("-------------------------git上修改");
 		setContentView(R.layout.activity_main);
 	}
 
